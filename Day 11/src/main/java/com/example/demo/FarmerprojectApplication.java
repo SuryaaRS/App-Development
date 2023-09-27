5c9a7b23c384d6ae5d3dd06be4ef2c46b3f4a0f0
SuryaaRS <120247114+SuryaaRS@users.noreply.github.com>
2023-09-27 10:52:30 +0530

    Add files via upload

---
+++ Day 11/src/main/java/com/example/demo/FarmerprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerprojectApplication.class, args);
+	}
+
+}
---
+++ Day 11/src/main/java/com/example/demo/model/model.java
@@ -0,0 +1,56 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class model {
+        @Id
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
+        private int id;
+        private int price;
+        private  String name;
+        private String category;
+        
+		public model()
+		{
+			
+		}
+		public model(int id, int price, String name, String category) {
+			super();
+			this.id = id;
+			this.price = price;
+			this.name = name;
+			this.category = category;
+		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public int getPrice() {
+			return price;
+		}
+		public void setPrice(int price) {
+			this.price = price;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getCategory() {
+			return category;
+		}
+		public void setCategory(String category) {
+			this.category = category;
+		}
+        
+        
+}
