bebffc663775109392c928c000c5112456f7b299
SuryaaRS <120247114+SuryaaRS@users.noreply.github.com>
2023-10-09 11:35:27 +0530

    Delete Day 15/Day13 directory

--- Day 15/Day13/src/main/java/com/example/main/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.main.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Integer>{
-
-
-}
